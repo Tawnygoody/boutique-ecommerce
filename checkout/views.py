cefaf42c805eb024ba69d60d44d10cc23b314c4b
Tawnygoody <chrisgoodfellow1@hotmail.com>
2021-08-04 09:59:33 +0000

    secret key and public key stripe variables added

--- checkout/views.py
+++ checkout/views.py
@@ -1,22 +1,30 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 # Create your views here.
 
 
 def checkout(request):
     bag = request.session.get('bag', {})
-    if not bag: 
+    if not bag:
         message.error(request, "Theres nothing in your bag at the moment")
         return redirect(reverse('products'))
 
-
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JKfwRLOvkv1I5Gaqh6SpEF98gGu1PgLJFdeQ10CN8uwRfLKVKHf7JPUOVRzYI8yjwFMJwdbzM5K57Bwnw1qoFnQ00Af3nkJIW',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
